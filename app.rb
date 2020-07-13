37724dbcd090ce7a13767c79897f7db9b1a92812
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-07-12 17:13:22 -0700

    Done.

--- app.rb
+++ app.rb
@@ -6,4 +6,13 @@ class App < Sinatra::Base
         erb :newteam
     end
 
+    post '/team' do
+        @name = params[:name]
+        @coach = params[:coach]
+        @pg = params[:pg]
+        @sg = params[:sg]
+        @pf = params[:pf]
+        @center = params[:c]
+        erb :team
+    end
 end
